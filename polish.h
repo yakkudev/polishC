fd72865d34306071932c0bd31e34f93ecd3a0d2c
yakkudev <65334490+yakkudev@users.noreply.github.com>
2023-03-17 13:20:52 +0100

    Commited a warcrime

---
+++ polish.h
@@ -0,0 +1,80 @@
+#define nic NULL
+
+#define oznakowana signed
+#define nieoznakowana unsigned
+#define statyczna static
+#define niepewna volatile
+#define stała const
+#define automatyczna auto
+#define struktura struct
+#define unijna union
+#define rejestrowana register
+#define zewnętrzna extern
+#define deklaruj typedef
+
+#define wyliczona enum
+#define pustka void
+#define znak char
+#define całkowita int
+#define krótka short
+#define długa long
+#define zmiennoprzecinkowa float
+#define podwójna double
+#define logiczna bool
+
+#define jeżeli if
+#define inaczej else
+#define kiedy while
+#define zrób do
+#define dla for
+#define kontynuuj continue;
+#define przełącznik switch
+#define wypadek case
+#define przerwij break
+#define domyślnie default
+
+#define prawda true
+#define fałsz false
+
+#define zwróć return
+
+#define rozmiar sizeof
+#define idź goto
+
+// stdio.h
+#define drukuj printf
+#define skanuj scanf
+
+#define otwórz fopen
+#define zapisz fprintf
+#define czytaj fgets
+#define zamknij fclose
+
+// string.h
+#define łączciąg strcat
+#define kopiujciąg strcpy
+#define duplikujciąg strdup
+#define porównajciąg strcmp
+#define zmierzciąg strlen
+#define małyciąg strlwr
+#define dużyciąg strupr
+#define odwróćciąg strrev
+
+#define wstaw puts
+#define bierz gets
+
+// math.h
+#define bezwzględna abs
+#define bezwzgl abs
+#define potęga power
+#define pot floor
+#define pierwiastekkwadratowy sqrt
+#define pierwkwadr sqrt
+#define pierw sqrt
+#define pierwiasteksześcienny cbrt
+#define pierwścian cbrt
+#define zaokrąglijwgórę ceil
+#define zaokrgóra ceil
+#define zaokrąglijwdół floor
+#define zaokrdół floor
+
